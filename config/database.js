ce9bb9f9f04f59fedb821e888744db85daac0b8d
cuong tran <cuong.it.533@gmail.com>
2023-06-13 16:31:21 +0700

    ta

--- config/database.js
+++ config/database.js
@@ -4,12 +4,12 @@ dotenv.config();
 
 module.exports = {
     development: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        port: process.env.DB_PORT,
-        host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT,
+        username: "root",
+        password: 123456,
+        database: contactdemo,
+        port: 3308,
+        host: "localhost",
+        dialect: "mysql",
     },
     test: {
         username: process.env.DB_USER,
@@ -19,10 +19,11 @@ module.exports = {
         dialect: process.env.DB_DIALECT,
     },
     production: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT,
+        username: "root",
+        password: 123456,
+        database: contactdemo,
+        port: 3308,
+        host: "localhost",
+        dialect: "mysql",
     },
 };
